f73afde517eb22b2d155a4412316e3393a4fa34e
Crtruban <55709830+Crtruban@users.noreply.github.com>
2025-03-28 04:46:20 +0000

    trailingSlash

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -12,12 +14,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
-		alias: {
-			"@/*": "./path/to/lib/*",
-		  },
-		  paths: {
-			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
-		  }
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH,
+		}
 	}
 };
 
