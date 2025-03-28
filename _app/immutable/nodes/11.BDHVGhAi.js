335516e3f0d7684d0c416f3cb7379be84c8737ae
Crtruban <55709830+Crtruban@users.noreply.github.com>
2025-03-28 04:24:26 +0000

    delete build

--- _app/immutable/nodes/11.BDHVGhAi.js
+++
@@ -1,1 +0,0 @@
-import"../chunks/CWj6FrbW.js";import"../chunks/69_IOA4Y.js";import{t as o,a as p}from"../chunks/DJnApY8F.js";import{a as m}from"../chunks/CROgWhsl.js";var e=o(`<img alt="Zaldara's Dungeon Map">`);function c(t){let r="/map.png";var a=e();m(a,"src",r),p(t,a)}export{c as component};
