a4271b361eae7eb2dde88f2776b3116620b69237
Camryn Truban <camtruban@gmail.com>
2025-03-28 09:09:07 -0400

    Kracken changes

--- src/routes/baseDungeon/krack/+page.svelte
+++ src/routes/baseDungeon/krack/+page.svelte
@@ -24,7 +24,15 @@
           Read as players enter
         </h5>
         <p>
-          The large stone door before you is carved in the image of a kraken.
+          The cavern walls glisten with moisture as you step
+          cautiously forward, bits of light from both caverns flicker against the jagged
+          stone. Before you looms a massive, ancient door, its weathered
+          surface carved with the writhing form of a kraken, its tentacles
+          stretching as if to ensnare those who dare approach. A pulsing blue
+          and purple aura seeps from the engravings, casting eerie shadows that
+          dance across the damp cave walls. The air hums with latent energy, and
+          a deep, resonant vibration echoes through the chamber—an unspoken
+          warning that the door is more than just stone and magic.
         </p>
       </Card>
     </div>
@@ -36,11 +44,11 @@
       <Card class="scroll-card">
         <ul>
           <li>
-            This door is locked. A detect magic spell reveals the door radiates an aura of
-            conjuration magic.<br/> A successful DC 15 Intelligence (Investigation)
-            check finds a small arcane rune carved within the image of the
-            kraken.<br/> A successful DC 15 Intelligence (Arcana) check reveals
-            scratching out the rune with a weapon or tool disables the trap.
+            This door is locked. A detect magic spell reveals the door radiates
+            an aura of conjuration magic.<br /> A successful DC 15 Intelligence
+            (Investigation) check finds a small arcane rune carved within the
+            image of the kraken.<br /> A successful DC 15 Intelligence (Arcana) check
+            reveals scratching out the rune with a weapon or tool disables the trap.
           </li>
         </ul>
       </Card>
@@ -55,16 +63,16 @@
         <ul>
           <li>
             The kraken image and rune appear on both sides of the door. Only one
-            rune needs to be scratched out to disable the trap. Or alternatively, 
-            if a player has the key gem from Z4, having this in proximity will have the same effect.
-             If neither are true, and the players approach initiate the trap. Once past the door, the players will head 
-             to Z5.
+            rune needs to be scratched out to disable the trap. Or
+            alternatively, if a player has the key gem from Z4, having this in
+            proximity will have the same effect. If neither are true, and the
+            players approach initiate the trap. Once past the door, the players
+            will head to Z5.
           </li>
         </ul>
       </Card>
       <br />
       <br />
-      
     </span>
     <span style="width: 40%; padding-right: 10%;">
       <Card class="scroll-card">
@@ -75,22 +83,18 @@
         </h5>
         <ul>
           <li>
-            The trap is triggered when a creature that is not
-            undead that touches the door, an attempt to pick the
-            door’s lock fails, or the door is attacked. Spectral
-            tentacles shoot out of the door and grab for the
-            creature triggered it. That creature must succeed on
-            a DC 12 Dexterity saving throw or it is restrained by
-            the tentacles. The tentacles can restrain up to five
-            creatures at once.<br/>
-            If the tentacles restrain a creature, that creature
-            takes 3 (1d6) necrotic damage at the start of its turn
-            as long as it is restrained in this way. The creature
-            can repeat the saving throw at the end of its turns,
-            ending the restrained condition on a success.<br/>
-            If the tentacles restrain no creature or take radiant
-            damage (AC 12, +2 to all saves), they disappear until
-            the trap is triggered again.
+            The trap is triggered when a creature that is not undead that
+            touches the door, an attempt to pick the door’s lock fails, or the
+            door is attacked. Spectral tentacles shoot out of the door and grab
+            for the creature triggered it. That creature must succeed on a DC 12
+            Dexterity saving throw or it is restrained by the tentacles. The
+            tentacles can restrain up to five creatures at once.<br />
+            If the tentacles restrain a creature, that creature takes 3 (1d6) necrotic
+            damage at the start of its turn as long as it is restrained in this way.
+            The creature can repeat the saving throw at the end of its turns, ending
+            the restrained condition on a success.<br />
+            If the tentacles restrain no creature or take radiant damage (AC 12,
+            +2 to all saves), they disappear until the trap is triggered again.
           </li>
         </ul>
       </Card>
