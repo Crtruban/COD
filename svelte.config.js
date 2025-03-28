739b123aa0f9a8ca76cf43ae1149d394e99d8989
Camryn Truban <camtruban@gmail.com>
2025-03-28 09:02:21 -0400

    dungeon changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,41 +1,41 @@
-// import adapter from '@sveltejs/adapter-auto';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-// const dev = process.argv.includes('dev');
-// const basePath = dev ? '' : process.env.BASE_PATH || ''; // Replace with your repository name
+const dev = process.argv.includes('dev');
+const basePath = dev ? '' : process.env.BASE_PATH || ''; // Replace with your repository name
 
 
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	// Consult https://svelte.dev/docs/kit/integrations
-// 	// for more information about preprocessors
-// 	preprocess: vitePreprocess(),
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-// 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-// 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-// 	adapter: adapter(),
-// 		paths: {
-// 			base: basePath,
-// 		}
-// 	}
-// };
-
-// export default config;
-
-import adapter from '@sveltejs/adapter-static';
-
-export default {
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+	adapter: adapter(),
+		paths: {
+			base: basePath,
+		}
 	}
 };
+
+export default config;
+
+// import adapter from '@sveltejs/adapter-static';
+
+// export default {
+// 	kit: {
+// 		adapter: adapter({
+// 			// default options are shown. On some platforms
+// 			// these options are set automatically — see below
+// 			pages: 'build',
+// 			assets: 'build',
+// 			fallback: undefined,
+// 			precompress: false,
+// 			strict: true
+// 		})
+// 	}
+// };
